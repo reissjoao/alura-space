849940efcc85b5ba8b98231fccd6455472367c59
Joao Reis <joaooreis@hotmail.com>
2023-01-02 17:48:49 -0300

    update footer

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -30,7 +30,7 @@ export default function Rodape() {
           <img src={instagram} alt="ícone do instagram" />
         </a>
       </div>
-      <p>Desenvolvido por Alura</p>
+      <p>Desenvolvido por João Reis</p>
     </footer>
   );
 }
